544e0995a12af5d3f174848f7d0ec00eacb7840f
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-06-13 13:43:49 +0700

    Fix min, max

--- src/main/java/net/danh/mmoxpaddon/Event/MMDeath.java
+++ src/main/java/net/danh/mmoxpaddon/Event/MMDeath.java
@@ -6,6 +6,7 @@ import net.Indyuce.mmocore.api.player.PlayerData;
 import net.Indyuce.mmocore.experience.EXPSource;
 import net.danh.dcore.Calculator.Calculator;
 import net.danh.mmoxpaddon.Resource.File;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,25 +17,23 @@ public class MMDeath implements Listener {
 
     @EventHandler
     public void onMythicMobDeath(MythicMobDeathEvent event) {
-        Player p = (Player) event.getKiller();
-        if (p == null) {
-            return;
-        }
-        String mobs = event.getMob().getType().getInternalName();
-        int player_level = PlayerData.get(p).getLevel();
-        int mob_level_max = File.getmobfile().getInt("MOBS." + mobs + ".LEVEL.MAX");
-        int mob_level_min = File.getmobfile().getInt("MOBS." + mobs + ".LEVEL.MIN");
-        int level = (int) event.getMobLevel();
-        int xp = Math.max(File.getconfigfile().getInt("XP-MIN"), File.getmobfile().getInt("MOBS." + mobs + ".XP"));
-        if (player_level > mob_level_min && player_level <= mob_level_max) {
-            PlayerData.get(p).giveExperience(level * xp, EXPSource.SOURCE);
-        } else {
-            String c = (Objects.requireNonNull(File.getconfigfile().getString("FORMULA")).replaceAll("%player_level%", String.valueOf(player_level)).replaceAll("%mob_level%", String.valueOf(level)).replaceAll("%mob_xp%", String.valueOf(xp)));
-            String c2 = PlaceholderAPI.setPlaceholders(p, c);
-            double formula = Double.parseDouble(Calculator.calculator(c2, -1));
-            int xpf = (int) formula;
-            PlayerData.get(p).giveExperience(xpf, EXPSource.SOURCE);
-        }
+        if (event.getKiller() instanceof Player p) {
+            String mobs = event.getMob().getType().getInternalName();
+            int player_level = PlayerData.get(p).getLevel();
+            int mob_level_max = File.getmobfile().getInt("MOBS." + mobs + ".LEVEL.MAX");
+            int mob_level_min = File.getmobfile().getInt("MOBS." + mobs + ".LEVEL.MIN");
+            int level = (int) event.getMobLevel();
+            int xp = Math.max(File.getconfigfile().getInt("XP-MIN"), File.getmobfile().getInt("MOBS." + mobs + ".XP"));
+            if (player_level >= mob_level_min && player_level <= mob_level_max) {
+                PlayerData.get(p).giveExperience(level * xp, EXPSource.SOURCE, event.getEntity().getLocation(), true);
+            } else {
+                String c = (Objects.requireNonNull(File.getconfigfile().getString("FORMULA")).replaceAll("%player_level%", String.valueOf(player_level)).replaceAll("%mob_level%", String.valueOf(level)).replaceAll("%mob_xp%", String.valueOf(xp)));
+                String c2 = PlaceholderAPI.setPlaceholders(p, c);
+                double formula = Double.parseDouble(Calculator.calculator(c2, -1));
+                int xpf = (int) formula;
+                PlayerData.get(p).giveExperience(xpf, EXPSource.SOURCE, event.getEntity().getLocation(), true);
+            }
 
+        }
     }
 }
