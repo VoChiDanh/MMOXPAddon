beb1e7dda26c6e402f565bb2eee1f43fe0a1b619
VoChiDanh <vochidanh.hs.c@gmail.com>
2022-06-14 11:13:00 +0700

    v1.6 - Fix the parentheses, add debug mode

--- src/main/java/net/danh/mmoxpaddon/Event/MMDeath.java
+++ src/main/java/net/danh/mmoxpaddon/Event/MMDeath.java
@@ -5,6 +5,7 @@ import me.clip.placeholderapi.PlaceholderAPI;
 import net.Indyuce.mmocore.api.player.PlayerData;
 import net.Indyuce.mmocore.experience.EXPSource;
 import net.danh.dcore.Calculator.Calculator;
+import net.danh.dcore.DCore;
 import net.danh.mmoxpaddon.Resource.File;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -13,6 +14,13 @@ import org.bukkit.event.Listener;
 import java.util.Objects;
 
 public class MMDeath implements Listener {
+
+
+    private void debug(String msg) {
+        if (File.getconfigfile().getBoolean("DEBUG")) {
+            DCore.dCoreLog(msg);
+        }
+    }
 
     @EventHandler
     public void onMythicMobDeath(MythicMobDeathEvent event) {
@@ -23,17 +31,29 @@ public class MMDeath implements Listener {
             int mob_level_min = File.getmobfile().getInt("MOBS." + mobs + ".LEVEL.MIN");
             int level = (int) event.getMobLevel();
             int xp = Math.max(File.getconfigfile().getInt("XP-MIN"), File.getmobfile().getInt("MOBS." + mobs + ".XP"));
+            debug("Player level: " + player_level);
+            debug("Mob level max: " + mob_level_max);
+            debug("Mob level min: " + mob_level_min);
+            debug("Mob level: " + level);
+            debug("Mob Name: " + mobs);
+            debug("XP: " + xp);
             if (player_level >= mob_level_min && player_level <= mob_level_max) {
                 String c = (Objects.requireNonNull(File.getconfigfile().getString("FORMULA.WITHIN_LIMITS")).replaceAll("%player_level%", String.valueOf(player_level)).replaceAll("%mob_level%", String.valueOf(level)).replaceAll("%mob_xp%", String.valueOf(xp)));
                 String c2 = PlaceholderAPI.setPlaceholders(p, c);
+                debug("Formula:" + c2);
                 double formula = Double.parseDouble(Calculator.calculator(c2, -1));
+                debug("Final XP:" + formula);
                 int xpf = (int) formula;
+                debug("XP give: " + xpf);
                 PlayerData.get(p).giveExperience(level * xpf, EXPSource.SOURCE, event.getEntity().getLocation().add(0, 1.5, 0), true);
             } else {
                 String c = (Objects.requireNonNull(File.getconfigfile().getString("FORMULA.OUT_OF_BOUNDS")).replaceAll("%player_level%", String.valueOf(player_level)).replaceAll("%mob_level%", String.valueOf(level)).replaceAll("%mob_xp%", String.valueOf(xp)));
                 String c2 = PlaceholderAPI.setPlaceholders(p, c);
-                double formula = Double.parseDouble(Calculator.calculator(c2, -1));
+                debug("Formula:" + c2);
+                double formula = Double.parseDouble(Calculator.calculator(c2, 0));
+                debug("Final XP:" + formula);
                 int xpf = (int) formula;
+                debug("XP give: " + xpf);
                 PlayerData.get(p).giveExperience(xpf, EXPSource.SOURCE, event.getEntity().getLocation().add(0, 1.5, 0), true);
             }
         }
